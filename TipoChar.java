95c79fc60eb525af8621400129d0077f42b8aba3
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-09-01 08:58:34 -0300

    Variavel do tipo char

---
+++ TipoChar.java
@@ -0,0 +1,11 @@
+public class TipoChar {
+    public static void main(String[] args){
+        char caracter = 'A';
+        char segundoCaracter = 95;
+        char terceiroCaracter = '\u0041';
+
+        System.out.println(caracter);
+        System.out.println(segundoCaracter);
+        System.out.println(terceiroCaracter);
+    }
+}
