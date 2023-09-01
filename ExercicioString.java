f577c9263ec288a7ba1fe23cd5cbad4ccc051d07
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-09-01 08:58:52 -0300

    Exercicio de String com concatenação

---
+++ ExercicioString.java
@@ -0,0 +1,11 @@
+public class ExercicioString {
+    public static void main(String[] args) {
+        String nome = "Gabriel";
+        String endereço = "Rua São Francisco Xavier";
+        long telefone = 999999999;
+        int idade = 22;
+
+        System.out.println("O cliente " + nome + ", que reside no endereço " + endereço + 
+        " e possui número de telefone " +telefone+", tem idade de "+idade+ " anos.");
+    }
+}
