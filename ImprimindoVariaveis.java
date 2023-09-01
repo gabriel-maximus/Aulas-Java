9d10f13aba1040deef9890148e86a8fd3f9f83a5
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-08-31 23:36:16 -0300

    Primeiros programas desenvolvidos em java

---
+++ ImprimindoVariaveis.java
@@ -0,0 +1,7 @@
+public class ImprimindoVariaveis{
+    public static void main(String[] args){
+        int idadeDaFilha;
+        idadeDaFilha = 9;
+        System.out.println(idadeDaFilha);
+    }
+}
