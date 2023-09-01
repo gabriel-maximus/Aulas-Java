2c2ce61761316ea268d0a087f5d952b44abda3ad
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-09-01 08:57:44 -0300

    String + concatenação

---
+++ TipoString.java
@@ -0,0 +1,7 @@
+public class TipoString {
+    public static void main(String[] args) {
+        int idadeDaFilha = 9;
+        String nome = "Carina";
+        System.out.println("A idade da filha é: " + idadeDaFilha);
+    }
+}
