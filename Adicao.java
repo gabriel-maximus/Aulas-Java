f5cbaf2be84bfacbb26cd93e61b302215787efe5
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-09-06 10:12:59 -0300

    Exercício com operador de adição

--- Adicao.java
+++ Adicao.java
@@ -2,6 +2,9 @@ public class Adicao {
     public static void main(String[] args) {
         int numero1 = 10;
         int numero2 = 20;
-        System.out.println(numero1+numero2);
+        System.out.println("A soma é: "+ (numero1+numero2));
+        //ou
+        int soma = numero1+numero2;
+        System.out.println("A soma é: "+ soma);
     }
 }
