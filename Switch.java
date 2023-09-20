b8aaaf01ee4cccaed8a439bb4010c1ec7ad9aac7
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-09-20 10:21:00 -0300

    Estrutura de switch case

---
+++ Switch.java
@@ -0,0 +1,19 @@
+public class Switch {
+    public static void main(String[] args) {
+        int numero = 10;
+        switch(numero){
+            case 5:
+            System.out.println("Numero 5");
+            break;
+            case 10:
+            System.out.println("Número 10");
+            break;
+            case 15:
+            System.out.println("Numero 15");
+            break;
+            case 20:
+            System.out.println("Numero 20");
+            break;
+        }
+    }
+}
