90678de24a4de3825fe611ae1ab121217f5578a2
gabriel-maximus <gabrielmaximus80@gmail.com>
2023-12-02 15:16:56 -0300

    Adicionando botões e ações na janela gráfica

--- JanelaGrafica/src/main/java/br/uerj/eng/desc/janelagrafica/Janela.java
+++ JanelaGrafica/src/main/java/br/uerj/eng/desc/janelagrafica/Janela.java
@@ -1,14 +1,45 @@
 package br.uerj.eng.desc.janelagrafica;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
-public class Janela extends JFrame{
+public class Janela extends JFrame implements ActionListener{
+    
+    JButton jb = new JButton("Botão 1");
+    JButton jb2 = new JButton("Sair");
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        
+        if(e.getSource()==jb){
+            //Exibir uma mensagem na tela com o clique do botão
+            JOptionPane.showMessageDialog(null,"Funcionou!!!");
+        }
+        if(e.getSource()==jb2){
+            System.exit(0);
+        }
+    }
+    
     public Janela(){
         //JFrame jf = new JFrame(); //cria a janela
         setTitle("Título"); //seta o titulo da janela
-        setSize(500,400); //seta o tamanho da janela em pixels
+        setSize(700,600); //seta o tamanho da janela em pixels
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //o programa deve ser finalizado quando apertar x 
         setLocationRelativeTo(null); //seta que a janela deve aparecer centralizada
         setResizable(false); //a janela não poderá ser redimensionada
         setVisible(true); //torna a janela visivel
+        
+        //Criação do Botão
+        setLayout(null); //informa que vamos fornecer as informações do layout
+        jb.setBounds(200,300,100,60); //seta a posição do componente (x,y,largura,altura)
+        add(jb); //adicionar o botão na janela
+        jb.addActionListener(this); // adicionar ação ao botão
+        
+        //Criação do Segundo Botão;
+        jb2.setBounds(400,300,100,60);
+        add(jb2);
+        jb2.addActionListener(this);
     }
 }
